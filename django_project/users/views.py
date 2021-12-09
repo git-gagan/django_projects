b8e4d475e9d1114993f44485388693cfac1e8df6
Bunny <gaganrajdeepsingh009@gmail.com>
2021-12-09 15:38:10 +0530

    Updating profile done

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from .forms import MyForm
+from .forms import MyForm, ProfileUpdateForm, UserUpdateForm
 from django.contrib import messages
 
 def register(request):
@@ -17,4 +17,21 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f"Profile updated for user {request.user.username}")
+            return redirect("profile")
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+        
+    context = {
+        "u_form": user_form,
+        "p_form": profile_form
+    }
+    
+    return render(request, "users/profile.html", context)
