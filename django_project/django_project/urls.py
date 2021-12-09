5c1d3b9096150aefe029cfc3ecfe179ebae0007a
Bunny <gaganrajdeepsingh009@gmail.com>
2021-12-09 12:58:17 +0530

    User profile pic done and signals added

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import settings
 from django.urls.conf import include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,7 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
